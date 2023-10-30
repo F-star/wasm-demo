5218e1727859fda6bcce9755bc5773c515017519
xigua <hhbuhuiai@163.com>
2023-10-31 01:28:02 +0800

    feat: add.wasm

---
+++ add/add.wat
@@ -0,0 +1,8 @@
+(module
+  ;; 将两个 i32 类型的参数相加返回
+  (func (export "add") (param i32 i32) (result i32)
+    local.get 0
+    local.get 1
+    i32.add
+  )
+)
---
+++ add/index.js
@@ -0,0 +1,9 @@
+WebAssembly.instantiateStreaming(fetch('./add.wasm')).then(res => {
+  const { instance } = res;
+  const { add } = instance.exports;
+  console.log(add(100, 34));
+  console.log(add(100.233, 34)); // 浮点数被 add 转成整数了
+  console.log(add(false, 34)); // true 被转成 1，false 被转成 0
+  // ...
+});
+
